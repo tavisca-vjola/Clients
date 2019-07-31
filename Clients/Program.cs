75f01f317e5e6c183a6008db2c40806d477ba214
vjola <vjola@tavisca.com>
2019-07-31 14:17:11 +0530

    Add project files.

---
+++ Clients/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Clients
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            int port = 8080;
+            string ipaddress = "172.16.5.154";
+            Socket serverListner = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipaddress),port);
+            serverListner.Bind(ep);
+            serverListner.Listen(100);
+            Console.WriteLine("Server is Listening");
+            Socket clientsocket = default(Socket);
+            int counter = 0;
+            Server server= new Server();
+            while(true)
+            {
+                counter++;
+                clientsocket = serverListner.Accept();
+                Console.WriteLine(counter + "clients connected");
+                var s = Task.Factory.StartNew(()=>server.Recieve(clientsocket));
+                var p = Task.Factory.StartNew(()=>server.Send(clientsocket));
+                //Thread thread = new Thread(new ThreadStart(()=>server.Recieve(clientsocket)));
+              //  thread.Start(User);
+            }
+        } 
+        public void  Send(Socket client)
+        {
+            while(true)
+            {
+                string MsgfromServer = Console.ReadLine();
+                byte[] recieved = new byte[100];
+                recieved = System.Text.Encoding.UTF8.GetBytes(MsgfromServer);
+                client.Send(recieved, 0, MsgfromServer.Length, SocketFlags.None);
+            }
+        }
+        public void Recieve(Socket client)
+        {
+            while (true)
+            {
+                byte[] msg = new byte[100];
+                int size = client.Receive(msg);
+                Console.WriteLine(System.Text.Encoding.UTF8.GetString(msg));
+                
+            }
+
+        }
+    }
+}
---
+++ Clients/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Clients")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Clients")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c0be3f29-dc5d-42cc-9205-f85bdc8c9eb7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
